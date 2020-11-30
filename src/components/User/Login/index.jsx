05572ccb3c9999f7ce4d13e1b8b0329a5190ddb6
Santeri Einola <62256499+Santeriii@users.noreply.github.com>
2020-11-30 17:31:51 +0200

    user-functions added

--- src/components/User/Login/index.jsx
+++ src/components/User/Login/index.jsx
@@ -1,25 +1,55 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 import loginService from '../../../services/login'
-import { makeStyles } from '@material-ui/core/styles'
 
-const useStyles = makeStyles(theme => ({
-    text_center: {
-        color: 'white',
-        textAlign: 'center',
-        textShadow: '-2px 0 black, 0 2px black, 2px 0 black, 0 -2px black',
-    },
-    rootElement: theme.rootElement,
-    backgroundImage: theme.backgroundImage,
-    paper: theme.paper,
-    paragraph: {
-        fontSize: '70%',
-        fontFamily: 'Serif',
-    },
-}))
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-export default function Login() {
-    const classes = useStyles()
-    const [errorMessage, setErrorMessage]  = useState('')
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignIn() {
+  const classes = useStyles()
+  const [errorMessage, setErrorMessage]  = useState('')
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [user, setUser] = useState(null)
@@ -54,38 +84,78 @@ export default function Login() {
         }
     }
 
-    const loginForm = () => (
-        <form onSubmit={handleLogin}>
-          <div>
-            username
-              <input
-              type="text"
-              value={username}
-              name="Username"
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            password
-              <input
-              type="password"
-              value={password}
-              name="Password"
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
-          <button type="submit">login</button>
-        </form>      
-    )
-
-    return (
-        <div className={classes.rootElement}>
-            <h1 className={classes.paper}>
-                <h1 className={classes.text_center}>Login</h1>
-                <h1>{errorMessage}</h1>
-                {user === null && loginForm()}
-                {user !== null && <h1>Tervetuloa {user.name}!</h1>}
-            </h1>
-        </div>
-    )
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+      {user !== null && <h1>Tervetuloa {user.name}!</h1>}
+      {user === null &&
+      <>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={handleLogin}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            value={username}
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={({ target }) => setUsername(target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            value={password}
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+        </>
+        }
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
 }
