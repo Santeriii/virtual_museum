2d54251361418601bbb88bdaf785de32e648f4a0
Santeri Einola <62256499+Santeriii@users.noreply.github.com>
2020-12-02 23:00:31 +0200

    search-page design enhanced

--- frontend/src/components/Museum/RecordList/index.jsx
+++ frontend/src/components/Museum/RecordList/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+import Divider from '@material-ui/core/Divider'
 
 const useStyles = makeStyles(theme => ({
     paragraph: {
         fontSize: '90%',
         fontFamily: 'Serif',
+    },
+    divider: {
+        backgroundColor: 'white',
     },
 }))
     
@@ -25,6 +29,7 @@ export default function RecordList({ records }) {
                             <li className={classes.paragraph}>{building.translated}</li>
                         ))}
                         </ul>
+                        <Divider className={classes.divider}/>
                         </>
                     ))}
                 </>
