9e369fa044b4246662312ca32b90907f93a84d4a
Santeri Einola <62256499+Santeriii@users.noreply.github.com>
2020-12-09 11:58:02 +0200

    heading- and content-fields for commenting enhanced

--- frontend/src/components/Forum/components/NoteForm.js
+++ frontend/src/components/Forum/components/NoteForm.js
@@ -1,8 +1,31 @@
 import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+    content: {
+        width: '100%',
+        height: '10vh',
+        resize: 'none',
+        backgroundColor: '#f8f8f8',
+        padding: '12px 20px',
+        boxSizing: 'border-box',
+        border: '2px solid #ccc',
+        borderRadius: '4px',
+    },
+    heading: {
+        width: '40%',
+        backgroundColor: '#f8f8f8',
+        padding: '7px 14px',
+        boxSizing: 'border-box',
+        border: '2px solid #ccc',
+        borderRadius: '4px',
+    },
+})
 
 const NoteForm = ({ createNote }) => {
     const [newNote, setNewNote] = useState('')
     const [newHeading, setNewHeading] = useState('')
+    const classes = useStyles()
 
     const handleChange = event => {
         setNewNote(event.target.value)
@@ -30,10 +53,21 @@ const NoteForm = ({ createNote }) => {
 
             <form onSubmit={addNote}>
                 <p>
-                    otsikko: <input value={newHeading} onChange={handleHeadingChange} />
+                    otsikko:{' '}
+                    <input
+                        className={classes.heading}
+                        value={newHeading}
+                        onChange={handleHeadingChange}
+                    />
                 </p>
                 <p>
-                    sisältö: <input value={newNote} onChange={handleChange} />
+                    sisältö:
+                    <br />
+                    <textarea
+                        className={classes.content}
+                        value={newNote}
+                        onChange={handleChange}
+                    />
                 </p>
                 <button type="submit">tallenna</button>
             </form>
